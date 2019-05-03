b897389452c61d16022561330212e7d71e32d5d8
agnxr <ar21@o2.pl>
2019-05-03 22:47:31 +0200

    add Yielp api key

---
+++ src/util/Yelp.js
@@ -0,0 +1,10 @@
+const apiKey = 'TovHecvqzoEV_J6XhHA3k6wqTwjomgM7KOkmUtBfQYoC8p7wvJzw6m7R5Y4y_whx7mhWtLB3I9dgAlJ6N0JYlQ8sNztM-wggSEGEq8PZW3WFMShydML8CJI2xaTMXHYx';
+
+const yelp = {
+
+    searchYelp(term, location, sortBy) {
+        return fetch();
+    }
+
+};
+
