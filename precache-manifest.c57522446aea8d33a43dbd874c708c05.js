e3045a29b1147494dc9d6c3279c96959f2f9d257
agnxr <ar21@o2.pl>
2019-05-05 14:06:26 +0200

    Updates

---
+++ precache-manifest.c57522446aea8d33a43dbd874c708c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ba0d4458581d000f492f1adb1baaf5",
+    "url": "/finder/index.html"
+  },
+  {
+    "revision": "34ad4e90b9ceaff6b9ff",
+    "url": "/finder/static/css/main.4d37c6ef.chunk.css"
+  },
+  {
+    "revision": "9f0f47cc047dd2ddec08",
+    "url": "/finder/static/js/2.3d3e5e3a.chunk.js"
+  },
+  {
+    "revision": "34ad4e90b9ceaff6b9ff",
+    "url": "/finder/static/js/main.c804bfa6.chunk.js"
+  },
+  {
+    "revision": "9656adfbf2046970795c",
+    "url": "/finder/static/js/runtime~main.473f2b9b.js"
+  },
+  {
+    "revision": "288e34f4d74033bbcb7ed7f47a8ad30f",
+    "url": "/finder/static/media/logo.288e34f4.png"
+  }
+]);
